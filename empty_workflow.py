8a0543bbfed56ec66ecc1454117da3bf97245709
Yacouby <yacouby@iguazio.com>
2025-03-03 11:26:28 +0200

    .

--- empty_workflow.py
+++ empty_workflow.py
@@ -3,4 +3,4 @@ from kfp import dsl
 
 @dsl.pipeline(name="remote_pipeline", description="tests remote pipeline")
 def pipeline(context):
-    context.logger.info("Basic remote workflow executed successfully.")
+    print("Basic remote workflow executed successfully.")
