faed6559c0bb52828b892fbb68cc647d6d43cd06
Yacouby <yacouby@iguazio.com>
2025-03-03 11:56:25 +0200

    .

--- empty_workflow.py
+++ empty_workflow.py
@@ -3,5 +3,8 @@ from kfp import dsl
 
 @dsl.pipeline(name="remote_pipeline", description="tests remote pipeline")
 def pipeline(context):
-    project = mlrun.get_or_create_project("proj", "./")
-    print("Basic remote workflow executed successfully.")
+    op = dsl.ContainerOp(
+        name="print-message",
+        image="python:3.9",  # A simple Python image
+        command=["echo", "Hello, Kubeflow!"]
+    )
