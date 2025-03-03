1923ea9edbe95c7f7a9adc0db86f08847029053f
Yacouby <yacouby@iguazio.com>
2025-03-03 11:43:59 +0200

    .

--- empty_workflow.py
+++ empty_workflow.py
@@ -3,4 +3,5 @@ from kfp import dsl
 
 @dsl.pipeline(name="remote_pipeline", description="tests remote pipeline")
 def pipeline(context):
+    project = mlrun.get_or_create_project("proj", "./")
     print("Basic remote workflow executed successfully.")
